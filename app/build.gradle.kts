0859e7c50d069292e11fb50fbaa1b81d713eea65
KAN1409 <engkareemnasser@gmail.com>
2026-09-16 02:42:06 +0300

    build: bump passive awareness hardening to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kareem.awarex"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.0"
+        versionCode = 4
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
