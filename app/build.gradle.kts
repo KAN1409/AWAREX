5ff85fed2d58e9511cb9aad9e66e37511d30ecb7
KAN1409 <engkareemnasser@gmail.com>
2026-09-16 02:36:59 +0300

    build: bump passive awareness milestone to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kareem.awarex"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
