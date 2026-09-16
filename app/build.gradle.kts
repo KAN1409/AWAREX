ec0ff8a063d1fff918bd0fad759d0f2ae610d8b3
KAN1409 <engkareemnasser@gmail.com>
2026-09-16 03:36:36 +0300

    build: bump passive relevance gate to 0.2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.kareem.awarex"
         minSdk = 28
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.2.3"
+        versionCode = 7
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
